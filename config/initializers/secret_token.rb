99d670b1c4a06cf11b275b24eb9418b8504ca178
Débora Setton <debora.setton@infosimples.com.br>
2013-04-06 07:13:08 -0300

    Initial Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuckoo::Application.config.secret_key_base = '4bb383e4633a73cfb0939ca7b17530ab62bc647902dfb2e2d8f5fe1e58edc8debae24b1e922abc3bdd960626128b3ac39954377e39d573e84653c8c2b1607ef3'
